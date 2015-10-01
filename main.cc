40409e4e8bd956d0478327778f288143e675233b
Marc Singer <elf@buici.com>
2015-10-01 12:08:08 -0700

    Demo mode 0.

--- main.cc
+++ main.cc
@@ -65,7 +65,18 @@ HID::DeviceP open_cap () {
   return std::move (d);
 }
 
-void preamble (HID::Device* d) {
+int send_reset_motors (HID::Device* d) {
+  std::array<char,8> msg = { 0x1, 0x11 };
+  return HID::write (d, &msg[0], msg.size ());
+}
+
+int send_config_motor (HID::Device* d, int motor, int duty) {
+  std::array<char,8> msg = { 0x4, 0x10,
+                             char (motor), char (duty*255/100), char (0xff) };
+  return HID::write (d, &msg[0], msg.size ());
+}
+
+void send_preamble (HID::Device* d) {
   // Poll for version
   {
     std::string s = "\x02\x01\x02     ";
@@ -101,12 +112,8 @@ void op_l (ArgList& args) {
 void op_r (ArgList& args) {
   auto d = open_cap ();
   printf ("reset motors\n");
-  preamble (d.get ());
-  {
-    std::array<char,8> msg = { 0x1, 0x11 };
-    auto result = HID::write (d.get (), &msg[0], msg.size ());
-//      printf ("write %d\n", result);
-  }
+  send_preamble (d.get ());
+  send_reset_motors (d.get ());
 }
 
 void op_config (ArgList& args) {
@@ -120,17 +127,30 @@ void op_config (ArgList& args) {
     usage ();
 
   auto d = open_cap ();
-  preamble (d.get ());
-
-  char frequency = 0xff;
-
   printf ("set motor %d  duty %d%%\n", motor, duty);
-  {
-    std::array<char,8> msg = { 0x4, 0x10,
-                               char (motor), char (duty*255/100), frequency };
-    auto result = HID::write (d.get (), &msg[0], msg.size ());
-    //      printf ("write %d\n", result);
-  }
+  send_preamble (d.get ());
+  send_config_motor (d.get (), motor, duty);
+}
+
+void op_d_0 (ArgList& args) {
+  if (args.size () < 4)
+    usage ();
+
+  int time  = strtoul (args[2].c_str (), nullptr, 0);
+  int duty  = strtoul (args[3].c_str (), nullptr, 0);
+
+  if (time < 0 || duty < 0 || duty > 100)
+    usage ();
+
+  auto d = open_cap ();
+  send_preamble (d.get ());
+
+  while (true)
+    for (auto motor = 0; motor < 14; ++motor) {
+      send_reset_motors (d.get ());
+      send_config_motor (d.get (), motor, duty);
+      sleep (time);
+    }
 }
 
 int main (int argc, const char** argv)
@@ -155,6 +175,22 @@ int main (int argc, const char** argv)
     case 'r':
       op_r (args);
       break;
+    case 'd':
+      if (args.size () < 2)
+        usage ();
+      {
+        int demo = strtol (args[1].c_str (), nullptr, 0);
+        switch (demo) {
+        case 0:
+          op_d_0 (args);
+          break;
+        default:
+          usage ();
+          break;
+        }
+      }
+      break;
+
     default:
       printf ("unknown switch\n");
       break;
